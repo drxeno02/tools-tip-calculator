1fd088a89ba1014ef5d5296bc394b9ec812e1633
Leonard Tatum <ljtatum@hotmail.com>
2016-12-19 21:17:27 -0800

    system file updates

---
+++ app/build/generated/source/buildConfig/androidTest/crashlyticsTest/debug/com/blog/ljtatum/tipcalculator/debug/test/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.blog.ljtatum.tipcalculator.debug.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.blog.ljtatum.tipcalculator.debug.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "crashlyticsTest";
+  public static final int VERSION_CODE = 6;
+  public static final String VERSION_NAME = "1.0.5";
+  // Fields from build type: debug
+  public static final boolean DEBUG_MODE = true;
+  public static final String ENVIRONMENT = "test";
+}
