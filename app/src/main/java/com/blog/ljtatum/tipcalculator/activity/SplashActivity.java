b9e5b9d2565aae94e3bcb8a81e42dd77d2880057
Leonard Tatum <ljtatum@hotmail.com>
2016-12-19 21:13:40 -0800

    minor updates

---
+++ app/src/main/java/com/blog/ljtatum/tipcalculator/activity/SplashActivity.java
@@ -0,0 +1,38 @@
+package com.blog.ljtatum.tipcalculator.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.blog.ljtatum.tipcalculator.R;
+
+public class SplashActivity extends Activity {
+    private final int SPLASH_TIMER = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // Create an Intent that will start MainActivity
+                Intent intent = new Intent(SplashActivity.this,
+                        MainActivity.class);
+                startActivity(intent);
+
+                // End SplashActivity
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
---
+++ app/src/main/java/com/blog/ljtatum/tipcalculator/constants/Constants.java
@@ -0,0 +1,13 @@
+package com.blog.ljtatum.tipcalculator.constants;
+
+import com.blog.ljtatum.tipcalculator.BuildConfig;
+
+/**
+ * Created by LJTat on 12/18/2016.
+ */
+
+public class Constants {
+
+    // debuggable mode; true to see debug logs otherwise false
+    public static final boolean DEBUG = BuildConfig.DEBUG_MODE;
+}
