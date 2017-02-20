53cd2ee755f50e864fa4e825df7ff90a248282bf
Leonard Tatum <ljtatum@hotmail.com>
2017-02-19 16:41:18 -0800

    system file update

--- app/src/main/java/com/blog/ljtatum/tipcalculator/application/TipApplication.java
+++ app/src/main/java/com/blog/ljtatum/tipcalculator/application/TipApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.blog.ljtatum.tipcalculator.utils.AppRaterUtil;
 import com.google.firebase.FirebaseApp;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by LJTat on 1/28/2017.
@@ -14,11 +16,10 @@ public class TipApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         // instantiate push notifications
         FirebaseApp.initializeApp(this);
         // instantiate crashlytics
-        //Fabric.with(this, new Crashlytics());
+        Fabric.with(this, new Crashlytics());
         // instantiate app rating utility class
         new AppRaterUtil(this);
 
